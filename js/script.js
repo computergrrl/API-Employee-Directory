257a4324db5dafbf9fb19d3f1f7343bc30cbd221
computergrrl <anani.mowse@gmail.com>
2021-05-21 14:53:25 -0700

    Added content dynamically and styled halfway

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
- 
-  // const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-  // email, location, phone, dob &noinfo &nat=US`;
+ // const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+ // email, location, phone, dob &noinfo &nat=US`;
 
-  const urlAPI = `https://randomuser.me/api/?results=12&nat=US`;
-  
-  fetch(urlAPI)
-  .then(response => response.json )
-  .then(response => response.results)
-  .then(console.log(response));
+ const urlAPI = `https://randomuser.me/api/?results=12&nat=US`;
+ const grid = document.querySelector(".main-grid");
+
+ fetch(urlAPI)
+   .then(response => response.json())
+   .then(response => response.results)
+   // .then(console.log(response));
+   // .then(response => console.log(response))
+   .then(loopOverUsers);
+
+ function loopOverUsers(data) {
+   data.forEach((user, index) => {
+     grid.innerHTML += `
+      <div class="card">
+      <img src="${user.picture.large}"> 
+      <div class="user-info">
+      <h2>${user.name.first} ${user.name.last}</h2>
+      <p class="email">${user.email}</p>
+      <p class="city">${user.location.city}</p>
+      </div>
+      </div>
+      
+      `
+   })
+ };
